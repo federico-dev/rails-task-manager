485a68fe0fa0aa99636de2480fe4a997175a1517
Federico Lorenzon <lorenzon.federico@gmail.com>
2021-07-10 15:14:22 +0200

    adding create feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,19 @@ class TasksController < ApplicationController
     @tasks = Task.find(params[:id])
   end
 
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(params[:tasks])
+    @tasks.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  private
+
+  def tasks_params
+    params.require(:tasks).permit(:title, :details, :completed)
+  end
 end
